425b8bdb7d0472d6ba6e76f0cfba24370a5c65bc
Filip <kuna@antstudio.cz>
2025-10-27 14:56:04 +0100

    Update DrawerOverlay component to modify background styling, enhancing visual appearance with a new backdrop blur effect.

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -25,7 +25,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn('fixed inset-0 z-50 bg-black/80', className)}
+    className={cn('bg-white-smoke/60 fixed inset-0 z-50 backdrop-blur-sm', className)}
     {...props}
   />
 ));
