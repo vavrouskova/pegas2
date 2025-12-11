8eba431eb6e73dec189e455bb559d1c620c7d1f6
Filip <kuna@antstudio.cz>
2025-12-11 16:35:10 +0100

    refactor: update CookieSettingButton component for improved functionality
    - Change Button import to a shared component for better consistency
    - Update button variant from 'secondary' to 'primary' for enhanced visibility
    - Simplify button rendering by directly passing buttonText prop
    
    These changes enhance the usability and maintainability of the CookieSettingButton component.

--- src/components/cookie/CookieSettingButton.tsx
+++ src/components/cookie/CookieSettingButton.tsx
@@ -1,21 +1,19 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import React from 'react';
 import { showPreferences } from 'vanilla-cookieconsent';
 
-import { Button } from '@/components/ui/button';
+import Button from '@/components/_shared/Button';
 
 const CookieSettingButton = () => {
   const t = useTranslations();
 
   return (
     <Button
-      variant='secondary'
+      variant='primary'
+      buttonText={t('cookie.setting_button')}
       onClick={showPreferences}
-    >
-      {t('cookie.setting_button')}
-    </Button>
+    />
   );
 };
 
