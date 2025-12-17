8caf2e964b31f6a49b701a785e066d7152f4e2c1
Filip <kuna@antstudio.cz>
2025-12-17 10:15:29 +0100

    refactor: remove unused metadata generation from 404 error page
    - Eliminate the generateMetadata function from the NotFound component as it is no longer needed, streamlining the code.

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,18 +1,8 @@
-import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import Image from 'next/image';
 import Link from 'next/link';
 
 import { FormattedText } from '@/components/_shared/FormattedText';
-
-export async function generateMetadata(): Promise<Metadata> {
-  const t = await getTranslations('error_page');
-
-  return {
-    title: t('404.title'),
-    description: t('404.description'),
-  };
-}
 
 const NotFound = async () => {
   const t = await getTranslations('error_page');
@@ -36,16 +26,22 @@ const NotFound = async () => {
         />
 
         <div>
-          <p className='text-primary text-lg md:text-xl'>{t('404.description')}</p>
-          <div>
-            <span className='text-primary text-lg md:text-xl'>{t('404.back_to_home_text')}</span>
-            <Link
-              href='/'
-              className='link text-primary font-text text-lg md:text-xl'
-            >
-              {t('404.back_to_home_link')}
-            </Link>
-          </div>
+          <FormattedText
+            text={t('404.description')}
+            as='p'
+            className='text-primary text-lg md:text-xl'
+          />
+          <FormattedText
+            text={t('404.back_to_home_text')}
+            as='span'
+            className='text-primary text-lg md:text-xl'
+          />
+          <Link
+            href='/'
+            className='link text-primary font-text text-lg md:text-xl'
+          >
+            {t('404.back_to_home_link')}
+          </Link>
         </div>
       </div>
     </main>
