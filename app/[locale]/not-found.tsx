ac5ef7594824231481495453966da152ff6267a2
Filip <kuna@antstudio.cz>
2025-12-17 10:07:12 +0100

    feat: enhance 404 error page with improved layout and translations
    - Update NotFound component to include an image and formatted text for better user experience.
    - Add links for navigation back to the home page.
    - Update Czech translations for 404 error page to include new text elements.

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FormattedText } from '@/components/_shared/FormattedText';
 
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations('error_page');
@@ -11,10 +14,40 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-const NotFound = () => {
+const NotFound = async () => {
+  const t = await getTranslations('error_page');
+
   return (
-    <main className='relative z-10 bg-white'>
-      <h1>Not Found</h1>
+    <main className='bg-white-smoke relative z-10 flex min-h-[85vh] flex-col items-center justify-center px-4 py-20'>
+      <div className='flex flex-col items-center text-center'>
+        <Image
+          src='/images/404-feather.webp'
+          alt=''
+          width={300}
+          height={150}
+          className='w-[300px]'
+          priority
+        />
+
+        <FormattedText
+          text={t('404.title')}
+          as='h1'
+          className='mb-12.5 text-5xl'
+        />
+
+        <div>
+          <p className='text-primary text-lg md:text-xl'>{t('404.description')}</p>
+          <div>
+            <span className='text-primary text-lg md:text-xl'>{t('404.back_to_home_text')}</span>
+            <Link
+              href='/'
+              className='link text-primary font-text text-lg md:text-xl'
+            >
+              {t('404.back_to_home_link')}
+            </Link>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
